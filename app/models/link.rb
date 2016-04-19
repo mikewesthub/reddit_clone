dda1a53562f72b0ed13148793a1764a3ee8bad15
Mike West <westmd23@gmail.com>
2016-04-18 20:05:07 -0400

    dependent destroy for links and comments

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ActiveRecord::Base
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   accepts_nested_attributes_for :comments
   validates :user_id, presence: true
   validates :title, presence: true
