711fdfce678d82f84fdeac69802aac68d1950abd
Mike West <westmd23@gmail.com>
2016-04-19 09:30:48 -0400

    fix navbar for current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-   @user ||= User.where(id: session[:user_id]).first
+   @current_user ||= User.where(id: session[:user_id]).first
   end
 
   helper_method :current_user
